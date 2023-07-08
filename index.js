4fb92b4b4abc21511620f2cd35f26f1fbd733ca4
Fabiosdl <fabiosdl@outlook.com>
2023-07-08 21:03:48 +0100

    menu icon

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const menuOption = document.getElementById("menu-icon");
+const dropdownContent = document.querySelector(".menu-icon .dropdown-content");
 const projectsItem = document.getElementById("projects");
 const subDropdownContent = document.querySelector(".sub-dropdown-content");
 
-projectsItem.addEventListener("click", function() {
+menuOption.addEventListener("click", function() {
+  dropdownContent.style.display = dropdownContent.style.display === "none" ? "block" : "none";
+});
+
+projectsItem.addEventListener("click", function(event) {
+  event.stopPropagation(); // Prevent the click event from bubbling up to the menuOption click event
+
   subDropdownContent.style.display = subDropdownContent.style.display === "none" ? "block" : "none";
 });
+
+// Close the dropdown when clicking outside of it
+document.addEventListener("click", function(event) {
+  if (!menuOption.contains(event.target)) {
+    dropdownContent.style.display = "none";
+    subDropdownContent.style.display = "none";
+  }
+});
+
